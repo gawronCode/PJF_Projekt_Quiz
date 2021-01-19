725014115170f92eaec528c7b3f360bf4f7e1bed
gawronCode <gawronCode@gmail.com>
2021-01-19 20:32:05 +0100

    Add models: Quiz, Question, Answer

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Quiz(models.Model):
+    title = models.CharField(max_length=64)
+
+
+class Question(models.Model):
+    quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE)
+    contents = models.CharField(max_length=512)
+
+
+class Answer(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    contents = models.CharField(max_length=512)
+    is_true = models.BooleanField(default=False)
