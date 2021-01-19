3e1f3333b2955a79fc41b01237bcf999e1ea2562
gawronCode <gawronCode@gmail.com>
2021-01-19 18:05:31 +0100

    Initial commit

---
+++ PJF_Projekt_Quiz/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for PJF_Projekt_Quiz project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PJF_Projekt_Quiz.settings')
+
+application = get_wsgi_application()
