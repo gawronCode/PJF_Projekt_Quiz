5e8e4faf189a13fed3f45b65bb2a7051e39d69ef
gawronCode <gawronCode@gmail.com>
2021-01-19 21:55:20 +0100

    added quiz,question,answer models to admin site. Def __str__ added to models

--- polls/models.py
+++ polls/models.py
@@ -4,13 +4,22 @@ from django.db import models
 class Quiz(models.Model):
     title = models.CharField(max_length=64)
 
+    def __str__(self):
+        return self.title
+
 
 class Question(models.Model):
     quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE)
     contents = models.CharField(max_length=512)
+
+    def __str__(self):
+        return self.contents
 
 
 class Answer(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     contents = models.CharField(max_length=512)
     is_true = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.contents
